77a0384cd166b6d6ac3d0debbe6ff831fcdb630d
Choshim-h <2372921050@qq.com>
2019-12-06 10:04:54 +0800

    two

---
+++ js/index.js
@@ -0,0 +1,217 @@
+const vm = new Vue({
+    el: "#wrapper",
+    data: {
+        src: "img/Choshim.jpg",
+        todos: [
+            { text: "微信" },
+            { text: "通讯录" },
+            { text: "收藏" },
+            { text: "聊天文件" }
+
+        ],
+        lot: "更多",
+        tit: "发起群聊",
+        alist: [
+            {
+                poto: "img/Choshim.jpg",
+                name: "大佬互吹",
+                text: "账号绑定提醒",
+            },
+            {
+                poto: "img/ps.jpg",
+                name: "PS",
+                text: "账号绑定提醒"
+            },
+            {
+                poto: "img/tgs.jpg",
+                name: "听故事",
+                text: "账号绑定提醒"
+            },
+            {
+                poto: "img/mg.jpg",
+                name: "芒果TV",
+                text: "账号绑定提醒"
+            },
+            {
+                poto: "img/Choshim.jpg",
+                name: "大佬互吹",
+                text: "账号绑定提醒",
+            },
+            {
+                poto: "img/ps.jpg",
+                name: "PS",
+                text: "账号绑定提醒"
+            },
+            {
+                poto: "img/tgs.jpg",
+                name: "听故事",
+                text: "账号绑定提醒"
+            },
+            {
+                poto: "img/mg.jpg",
+                name: "芒果TV",
+                text: "账号绑定提醒"
+            },
+            {
+                poto: "img/Choshim.jpg",
+                name: "大佬互吹",
+                text: "账号绑定提醒",
+            },
+            {
+                poto: "img/ps.jpg",
+                name: "PS",
+                text: "账号绑定提醒"
+            },
+            {
+                poto: "img/tgs.jpg",
+                name: "听故事",
+                text: "账号绑定提醒"
+            },
+            {
+                poto: "img/mg.jpg",
+                name: "芒果TV",
+                text: "账号绑定提醒"
+            },
+            {
+                poto: "img/Choshim.jpg",
+                name: "大佬互吹",
+                text: "账号绑定提醒",
+            },
+            {
+                poto: "img/ps.jpg",
+                name: "PS",
+                text: "账号绑定提醒"
+            },
+            {
+                poto: "img/tgs.jpg",
+                name: "听故事",
+                text: "账号绑定提醒"
+            },
+            {
+                poto: "img/mg.jpg",
+                name: "芒果TV",
+                text: "账号绑定提醒"
+            },
+            {
+                poto: "img/Choshim.jpg",
+                name: "大佬互吹",
+                text: "账号绑定提醒",
+            },
+            {
+                poto: "img/ps.jpg",
+                name: "PS",
+                text: "账号绑定提醒"
+            },
+            {
+                poto: "img/tgs.jpg",
+                name: "听故事",
+                text: "账号绑定提醒"
+            },
+            {
+                poto: "img/mg.jpg",
+                name: "芒果TV",
+                text: "账号绑定提醒"
+            },
+            {
+                poto: "img/Choshim.jpg",
+                name: "大佬互吹",
+                text: "账号绑定提醒",
+            },
+            {
+                poto: "img/ps.jpg",
+                name: "PS",
+                text: "账号绑定提醒"
+            },
+            {
+                poto: "img/tgs.jpg",
+                name: "听故事",
+                text: "账号绑定提醒"
+            },
+            {
+                poto: "img/mg.jpg",
+                name: "芒果TV",
+                text: "账号绑定提醒"
+            },
+            {
+                poto: "img/Choshim.jpg",
+                name: "大佬互吹",
+                text: "账号绑定提醒",
+            },
+            {
+                poto: "img/ps.jpg",
+                name: "PS",
+                text: "账号绑定提醒"
+            },
+            {
+                poto: "img/tgs.jpg",
+                name: "听故事",
+                text: "账号绑定提醒"
+            },
+            {
+                poto: "img/mg.jpg",
+                name: "芒果TV",
+                text: "账号绑定提醒"
+            },
+            {
+                poto: "img/Choshim.jpg",
+                name: "大佬互吹",
+                text: "账号绑定提醒",
+            },
+            {
+                poto: "img/ps.jpg",
+                name: "PS",
+                text: "账号绑定提醒"
+            },
+            {
+                poto: "img/tgs.jpg",
+                name: "听故事",
+                text: "账号绑定提醒"
+            },
+            {
+                poto: "img/mg.jpg",
+                name: "芒果TV",
+                text: "账号绑定提醒"
+            },
+            {
+                poto: "img/Choshim.jpg",
+                name: "大佬互吹",
+                text: "账号绑定提醒",
+            },
+            {
+                poto: "img/ps.jpg",
+                name: "PS",
+                text: "账号绑定提醒"
+            },
+            {
+                poto: "img/tgs.jpg",
+                name: "听故事",
+                text: "账号绑定提醒"
+            },
+            {
+                poto: "img/mg.jpg",
+                name: "芒果TV",
+                text: "账号绑定提醒"
+            },
+            {
+                poto: "img/Choshim.jpg",
+                name: "大佬互吹",
+                text: "账号绑定提醒",
+            },
+            {
+                poto: "img/ps.jpg",
+                name: "PS",
+                text: "账号绑定提醒"
+            },
+            {
+                poto: "img/tgs.jpg",
+                name: "听故事",
+                text: "账号绑定提醒"
+            },
+            {
+                poto: "img/mg.jpg",
+                name: "芒果TV",
+                text: "账号绑定提醒"
+            }
+        ]
+    }
+})
