ad73cb8865561e30692b0d7fa90b89fab64f14ab
gramer <guo18792551504@outlook.com>
2019-12-18 19:43:02 +0800

    数据控制

---
+++ js/alist.js
@@ -0,0 +1,264 @@
+let alist = [
+    {
+        poto: "img/Choshim.jpg",
+        name: "大佬互吹",
+        text: "账号绑定提醒",
+        icon: "...",
+        content: [
+            {
+                type: 1,
+                chart: '我说的话0'
+            },
+            {
+                type: 2,
+                chart: '对方说的话0'
+            }
+        ]
+    },
+    {
+        poto: "img/ps.jpg",
+        name: "PS",
+        text: "账号绑定提醒",
+        icon: "...",
+        content: [
+            {
+                type: 1,
+                chart: '我说的话1'
+            },
+            {
+                type: 2,
+                chart: '对方说的话1'
+            }
+        ]
+    },
+    {
+        poto: "img/tgs.jpg",
+        name: "听故事",
+        text: "账号绑定提醒",
+        icon: "...",
+        content: [
+            {
+                type: 1,
+                chart: '我说的话2'
+            },
+            {
+                type: 2,
+                chart: '对方说的话2'
+            }
+        ]
+    },
+    {
+        poto: "img/mg.jpg",
+        name: "芒果TV",
+        text: "账号绑定提醒",
+        icon: "...",
+        content: [
+            {
+                type: 1,
+                chart: '我说的话'
+            },
+            {
+                type: 2,
+                chart: '对方说的话'
+            }
+        ]
+    },
+    {
+        poto: "img/Choshim.jpg",
+        name: "大佬互吹",
+        text: "账号绑定提醒",
+        icon: "...",
+        content: [
+            {
+                type: 1,
+                chart: '我说的话'
+            },
+            {
+                type: 2,
+                chart: '对方说的话'
+            }
+        ]
+    },
+    {
+        poto: "img/ps.jpg",
+        name: "PS",
+        text: "账号绑定提醒",
+        icon: "...",
+        content: [
+            {
+                type: 1,
+                chart: '我说的话'
+            },
+            {
+                type: 2,
+                chart: '对方说的话'
+            }
+        ]
+    },
+    {
+        poto: "img/tgs.jpg",
+        name: "听故事",
+        text: "账号绑定提醒",
+        icon: "...",
+        content: [
+            {
+                type: 1,
+                chart: '我说的话'
+            },
+            {
+                type: 2,
+                chart: '对方说的话'
+            }
+        ]
+    },
+    {
+        poto: "img/mg.jpg",
+        name: "芒果TV",
+        text: "账号绑定提醒",
+        icon: "...",
+        content: [
+            {
+                type: 1,
+                chart: '我说的话'
+            },
+            {
+                type: 2,
+                chart: '对方说的话'
+            }
+        ]
+    },
+    {
+        poto: "img/Choshim.jpg",
+        name: "大佬互吹",
+        text: "账号绑定提醒",
+        icon: "...",
+        content: [
+            {
+                type: 1,
+                chart: '我说的话'
+            },
+            {
+                type: 2,
+                chart: '对方说的话'
+            }
+        ]
+    },
+    {
+        poto: "img/ps.jpg",
+        name: "PS",
+        text: "账号绑定提醒",
+        icon: "...",
+        content: [
+            {
+                type: 1,
+                chart: '我说的话'
+            },
+            {
+                type: 2,
+                chart: '对方说的话'
+            }
+        ]
+    },
+    {
+        poto: "img/tgs.jpg",
+        name: "听故事",
+        text: "账号绑定提醒",
+        icon: "...",
+        content: [
+            {
+                type: 1,
+                chart: '我说的话'
+            },
+            {
+                type: 2,
+                chart: '对方说的话'
+            }
+        ]
+    },
+    {
+        poto: "img/mg.jpg",
+        name: "芒果TV",
+        text: "账号绑定提醒",
+        icon: "...",
+        content: [
+            {
+                type: 1,
+                chart: '我说的话'
+            },
+            {
+                type: 2,
+                chart: '对方说的话'
+            }
+        ]
+    },
+    {
+        poto: "img/Choshim.jpg",
+        name: "大佬互吹",
+        text: "账号绑定提醒",
+        icon: "...",
+        content: [
+            {
+                type: 1,
+                chart: '我说的话'
+            },
+            {
+                type: 2,
+                chart: '对方说的话'
+            }
+        ]
+    },
+    {
+        poto: "img/ps.jpg",
+        name: "PS",
+        text: "账号绑定提醒",
+        icon: "...",
+        content: [
+            {
+                type: 1,
+                chart: '我说的话'
+            },
+            {
+                type: 2,
+                chart: '对方说的话'
+            }
+        ]
+    },
+    {
+        poto: "img/tgs.jpg",
+        name: "听故事",
+        text: "账号绑定提醒",
+        icon: "...",
+        content: [
+            {
+                type: 1,
+                chart: '我说的话'
+            },
+            {
+                type: 2,
+                chart: '对方说的话'
+            }
+        ]
+    },
+    {
+        poto: "img/mg.jpg",
+        name: "芒果TV",
+        text: "账号绑定提醒",
+        icon: "...",
+        content: [
+            {
+                type: 1,
+                chart: '我说的话'
+            },
+            {
+                type: 2,
+                chart: '对方说的话'
+            }
+        ]
+    },
+];
+let list = [
+    { tips: "表情" },
+    { tips: "文件" },
+    { tips: "截图" },
+    { tips: "聊天" }
+];
--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+
+let timeout = null;
+
 const vm = new Vue({
     el: "#wrapper",
     data: {
@@ -6,217 +9,25 @@ const vm = new Vue({
             { text: "微信" },
             { text: "通讯录" },
             { text: "收藏" },
-            { text: "聊天文件" }
-
+            { text: "聊天文件" },
         ],
         lot: "更多",
         tit: "发起群聊",
-        alist: [
-            {
-                poto: "img/Choshim.jpg",
-                name: "大佬互吹",
-                text: "账号绑定提醒",
-            },
-            {
-                poto: "img/ps.jpg",
-                name: "PS",
-                text: "账号绑定提醒"
-            },
-            {
-                poto: "img/tgs.jpg",
-                name: "听故事",
-                text: "账号绑定提醒"
-            },
-            {
-                poto: "img/mg.jpg",
-                name: "芒果TV",
-                text: "账号绑定提醒"
-            },
-            {
-                poto: "img/Choshim.jpg",
-                name: "大佬互吹",
-                text: "账号绑定提醒",
-            },
-            {
-                poto: "img/ps.jpg",
-                name: "PS",
-                text: "账号绑定提醒"
-            },
-            {
-                poto: "img/tgs.jpg",
-                name: "听故事",
-                text: "账号绑定提醒"
-            },
-            {
-                poto: "img/mg.jpg",
-                name: "芒果TV",
-                text: "账号绑定提醒"
-            },
-            {
-                poto: "img/Choshim.jpg",
-                name: "大佬互吹",
-                text: "账号绑定提醒",
-            },
-            {
-                poto: "img/ps.jpg",
-                name: "PS",
-                text: "账号绑定提醒"
-            },
-            {
-                poto: "img/tgs.jpg",
-                name: "听故事",
-                text: "账号绑定提醒"
-            },
-            {
-                poto: "img/mg.jpg",
-                name: "芒果TV",
-                text: "账号绑定提醒"
-            },
-            {
-                poto: "img/Choshim.jpg",
-                name: "大佬互吹",
-                text: "账号绑定提醒",
-            },
-            {
-                poto: "img/ps.jpg",
-                name: "PS",
-                text: "账号绑定提醒"
-            },
-            {
-                poto: "img/tgs.jpg",
-                name: "听故事",
-                text: "账号绑定提醒"
-            },
-            {
-                poto: "img/mg.jpg",
-                name: "芒果TV",
-                text: "账号绑定提醒"
-            },
-            {
-                poto: "img/Choshim.jpg",
-                name: "大佬互吹",
-                text: "账号绑定提醒",
-            },
-            {
-                poto: "img/ps.jpg",
-                name: "PS",
-                text: "账号绑定提醒"
-            },
-            {
-                poto: "img/tgs.jpg",
-                name: "听故事",
-                text: "账号绑定提醒"
-            },
-            {
-                poto: "img/mg.jpg",
-                name: "芒果TV",
-                text: "账号绑定提醒"
-            },
-            {
-                poto: "img/Choshim.jpg",
-                name: "大佬互吹",
-                text: "账号绑定提醒",
-            },
-            {
-                poto: "img/ps.jpg",
-                name: "PS",
-                text: "账号绑定提醒"
-            },
-            {
-                poto: "img/tgs.jpg",
-                name: "听故事",
-                text: "账号绑定提醒"
-            },
-            {
-                poto: "img/mg.jpg",
-                name: "芒果TV",
-                text: "账号绑定提醒"
-            },
-            {
-                poto: "img/Choshim.jpg",
-                name: "大佬互吹",
-                text: "账号绑定提醒",
-            },
-            {
-                poto: "img/ps.jpg",
-                name: "PS",
-                text: "账号绑定提醒"
-            },
-            {
-                poto: "img/tgs.jpg",
-                name: "听故事",
-                text: "账号绑定提醒"
-            },
-            {
-                poto: "img/mg.jpg",
-                name: "芒果TV",
-                text: "账号绑定提醒"
-            },
-            {
-                poto: "img/Choshim.jpg",
-                name: "大佬互吹",
-                text: "账号绑定提醒",
-            },
-            {
-                poto: "img/ps.jpg",
-                name: "PS",
-                text: "账号绑定提醒"
-            },
-            {
-                poto: "img/tgs.jpg",
-                name: "听故事",
-                text: "账号绑定提醒"
-            },
-            {
-                poto: "img/mg.jpg",
-                name: "芒果TV",
-                text: "账号绑定提醒"
-            },
-            {
-                poto: "img/Choshim.jpg",
-                name: "大佬互吹",
-                text: "账号绑定提醒",
-            },
-            {
-                poto: "img/ps.jpg",
-                name: "PS",
-                text: "账号绑定提醒"
-            },
-            {
-                poto: "img/tgs.jpg",
-                name: "听故事",
-                text: "账号绑定提醒"
-            },
-            {
-                poto: "img/mg.jpg",
-                name: "芒果TV",
-                text: "账号绑定提醒"
-            },
-            {
-                poto: "img/Choshim.jpg",
-                name: "大佬互吹",
-                text: "账号绑定提醒",
-            },
-            {
-                poto: "img/ps.jpg",
-                name: "PS",
-                text: "账号绑定提醒"
-            },
-            {
-                poto: "img/tgs.jpg",
-                name: "听故事",
-                text: "账号绑定提醒"
-            },
-            {
-                poto: "img/mg.jpg",
-                name: "芒果TV",
-                text: "账号绑定提醒"
-            }
-        ]
+        alist,
+        list,
+        search: '',
+        whoIndex: 0
     },
-    methods:{
-        focus() {
-            
-        }
+    mounted() {
+        this.handleSearch();
+    },
+    methods: {
+        handleSearch() {
+            clearTimeout(timeout);
+            timeout = setTimeout(() => {
+                const search = this.search.trim();
+                this.alist = (search ? alist.filter(item => item.name.indexOf(search) != -1) : alist);
+            }, 100)
+        },
     }
 })
