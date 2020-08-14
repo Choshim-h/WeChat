be3d2830140baac60746076c9fe0518f1967a392
Choshim-h <2372921050@qq.com>
2020-08-14 11:35:48 +0800

    end

---
+++ js/alist.js
@@ -0,0 +1,129 @@
+let alist = [
+    {
+        poto: "img/Choshim.jpg",
+        name: "大佬互吹",
+        text: "账号绑定提醒",
+        icon: "..."
+    },
+    {
+        poto: "img/ps.jpg",
+        name: "PS",
+        text: "账号绑定提醒",
+        icon: "..."
+    },
+    {
+        poto: "img/tgs.jpg",
+        name: "听故事",
+        text: "账号绑定提醒",
+        icon: "..."
+    },
+    {
+        poto: "img/mg.jpg",
+        name: "芒果TV",
+        text: "账号绑定提醒",
+        icon: "..."
+    },
+    {
+        poto: "img/Choshim.jpg",
+        name: "大佬互吹",
+        text: "账号绑定提醒",
+        icon: "...",
+    },
+    {
+        poto: "img/ps.jpg",
+        name: "PS",
+        text: "账号绑定提醒",
+        icon: "..."
+    },
+    {
+        poto: "img/tgs.jpg",
+        name: "听故事",
+        text: "账号绑定提醒",
+        icon: "..."
+    },
+    {
+        poto: "img/mg.jpg",
+        name: "芒果TV",
+        text: "账号绑定提醒",
+        icon: "..."
+    },
+    {
+        poto: "img/Choshim.jpg",
+        name: "大佬互吹",
+        text: "账号绑定提醒",
+        icon: "...",
+    },
+    {
+        poto: "img/ps.jpg",
+        name: "PS",
+        text: "账号绑定提醒",
+        icon: "..."
+    },
+    {
+        poto: "img/tgs.jpg",
+        name: "听故事",
+        text: "账号绑定提醒",
+        icon: "..."
+    },
+    {
+        poto: "img/mg.jpg",
+        name: "芒果TV",
+        text: "账号绑定提醒",
+        icon: "..."
+    },
+    {
+        poto: "img/Choshim.jpg",
+        name: "大佬互吹",
+        text: "账号绑定提醒",
+        icon: "...",
+    },
+    {
+        poto: "img/ps.jpg",
+        name: "PS",
+        text: "账号绑定提醒",
+        icon: "..."
+    },
+    {
+        poto: "img/tgs.jpg",
+        name: "听故事",
+        text: "账号绑定提醒",
+        icon: "..."
+    },
+    {
+        poto: "img/mg.jpg",
+        name: "芒果TV",
+        text: "账号绑定提醒",
+        icon: "..."
+    },
+    {
+        poto: "img/Choshim.jpg",
+        name: "大佬互吹",
+        text: "账号绑定提醒",
+        icon: "...",
+    },
+    {
+        poto: "img/ps.jpg",
+        name: "PS",
+        text: "账号绑定提醒",
+        icon: "..."
+    },
+    {
+        poto: "img/tgs.jpg",
+        name: "听故事",
+        text: "账号绑定提醒",
+        icon: "..."
+    },
+    {
+        poto: "img/mg.jpg",
+        name: "芒果TV",
+        text: "账号绑定提醒",
+        icon: "..."
+    }
+
+];
+let list = [
+    { tips: "表情" },
+    { tips: "文件" },
+    { tips: "截图" },
+    { tips: "聊天" }
+];
